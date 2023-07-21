0f87e09a401108eb8aff8b92c0113b462e56c8d2
mudatz <137837980+mudatz@users.noreply.github.com>
2023-07-21 16:15:20 -0500

    take out api key

--- chatgpt.py
+++ chatgpt.py
@@ -13,7 +13,7 @@ from langchain.vectorstores import Chroma
 
 import constants
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "api-key-goes-here"
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
 PERSIST = False
